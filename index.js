6ab5a5d9653e48e138c0688e20e947cef561944a
Dylan Cole <66389199+dyudis314@users.noreply.github.com>
2022-03-28 19:46:38 -0700

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
 const importData = require("./data.json");
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.get("/", (req, res) => {
     res.send("Hello World")
